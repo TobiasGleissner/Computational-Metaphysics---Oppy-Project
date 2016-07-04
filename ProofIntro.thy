26a54b38a787fed8217bd6740ffb3059332c8d6f
timpauli <timpauli@mi.fu-berlin.de>
2016-07-04 18:28:25 +0200

    beginning of maydole

--- ProofIntro.thy
+++ ProofIntro.thy
@@ -5,7 +5,7 @@ begin
 (*>*)
 
 abbreviation entails   :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> (\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (infix "\<^enum>" 50)
-  where "P \<^enum> Q  \<equiv> \<^bold>\<box>(\<^bold>\<forall>x. (P x) \<^bold>\<rightarrow> (Q x)) "
+  where "P \<^enum> Q  \<equiv> \<^bold>\<box>(\<^bold>\<forall>x. (P x) \<^bold>\<rightarrow> (Q x))"
 
 abbreviation  closedunderentailment::  "((\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("closed")
   where "closed (P) \<equiv> \<^bold>\<forall>R. \<^bold>\<forall>Q. (P (Q) \<^bold>\<and> (Q \<^enum> R)) \<^bold>\<rightarrow> P(R)"
---
+++ maydole.thy
@@ -0,0 +1,20 @@
+(*<*)
+theory maydole
+imports ProofIntro
+begin
+(*>*)
+consts greaterthan :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>"
+consts perfect :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"
+
+abbreviation supreme :: "\<mu> \<Rightarrow> \<sigma>"
+  where "supreme x  \<equiv> \<^bold>\<box>(\<^bold>\<forall>y. \<^bold>\<not>(meq x y) \<^bold>\<rightarrow> (greaterthan x y))"
+
+axiomatization 
+  where A1: "\<lfloor>\<^bold>\<forall>P. perfect P \<^bold>\<rightarrow> \<^bold>\<not> perfect (\<^sup>\<not> P)\<rfloor>"
+  and A2: "\<lfloor>closed perfect\<rfloor>"
+  and A3: "\<lfloor>perfect supreme\<rfloor>"
+
+theorem "\<lfloor>\<^bold>\<exists>x. supreme x \<rfloor>" 
+(*<*)
+end
+(*>*)
